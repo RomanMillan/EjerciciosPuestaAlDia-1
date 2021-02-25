5090cc6fe7e94b0c33afd1801c2947d29e3b7b2f
RomanMillan <74763033+RomanMillan@users.noreply.github.com>
2021-02-25 08:58:30 +0100

    Añadiendo trabajo rectángulo

---
+++ src/ejercicios/Rectangulo.java
@@ -0,0 +1,40 @@
+package ponerseAlDiaJavaRoman;
+
+public class Rectangulo {
+	private int longitud= 1;
+	private int ancho = 1;
+	
+	public Rectangulo(int lonitud, int ancho) {
+		super();
+		this.longitud = lonitud;
+		this.ancho = ancho;
+	}
+
+	public int getLonitud() {
+		return longitud;
+	}
+
+	public void setLonitud(int longitud) {
+		if (longitud > 1 || longitud <20) {
+			this.longitud = longitud;
+		}
+	}
+
+	public int getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(int ancho) {
+		if (ancho > 1 || ancho < 20) {
+			this.ancho = ancho;
+		}
+	}
+	
+	public int perimetro(int longitud, int ancho) {
+		return (this.longitud + this.ancho) * 2; 
+	}
+	
+	public int area(int longitud, int ancho) {
+		return this.longitud * this.ancho;
+	}
+}
