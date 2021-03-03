bd681f2a3e9c7cbefc8c24cda345f23ffd29ef48
roman millan <romandos962m@gmail.com>
2021-03-03 22:46:21 +0100

    cuenta mejorada

--- src/ejercicios/Cuenta.java
+++ src/ejercicios/Cuenta.java
@@ -2,34 +2,28 @@ package ejercicios;
 
 public class Cuenta {
 	private double saldo;
-	private double ncc;
-	private double numIngresos;
-	private double numReintegros;
+	private String ncc;
+	private int numIngresos;
+	private int numReintegros;
 	
 	public Cuenta(String ncc) {
 		
 	}
 	public Cuenta(double saldo, String ncc) throws Exception {
 		super();
-		if (saldo <0 || saldo == 0) {
-			throw new Exception();
-			
-		}else {
-			this.saldo = saldo;
-			this.numIngresos = 1;
+		if (saldo <=0) {
+			throw new Exception("Saldo no permitido");
 		}
-	}
-	
-
-		@Override
-		public String toString() {
-			// TODO Auto-generated method stub
-			return super.toString();
+		this.saldo = saldo;
+		this.numIngresos = 1;
+		this.ncc = ncc;
+		this.numReintegros = 0;
 		}
+		
 		public double getSaldo() {
 			return saldo;
 		}
-		public double getNcc() {
+		public String getNcc() {
 			return ncc;
 		}
 		public double getNumIngresos() {
@@ -39,12 +33,25 @@ public class Cuenta {
 			return numReintegros;
 		};
 		
-	public void reintegro(double cantidad) {
+	public void reintegro(double cantidad) throws Exception {
+		if (saldo < cantidad || cantidad <=0) {
+			throw new Exception("Error de cantidad");
+		}
 		saldo = saldo - cantidad;
 		this.numReintegros++;
-	}
-	public void ingresos (double cantidad) {
+	} 
+	public void ingresos (double cantidad)throws Exception {
+		if (cantidad <=0) {
+			throw new Exception("Error de cantidad");
+		}
 		saldo = saldo + cantidad;
 		this.numIngresos++;
 	}
+	
+	@Override
+	public String toString() {
+		return "Cuenta [saldo=" + saldo + ", ncc=" + ncc + ", numIngresos=" + numIngresos + ", numReintegros="
+				+ numReintegros + "]";
+	}
+	
 }
--- src/ejercicios/Principal.java
+++ src/ejercicios/Principal.java
@@ -4,33 +4,62 @@ import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner(System.in);
-		Cuenta cb = new Cuenta(20,"5454");
-		
-		System.out.println("1 Hacer un reintegro. \n"
-				+ "2 Hacer un ingreso \n"
-				+ "3 Consultar el saldo y número de reintegros e ingresos. \n"
-				+ "4 Finalizar las operaciones");
-		int elec = teclado.nextInt();
-		while(elec != 4) {
-			if (elec == 1) {
-				double result = cb.getSaldo();
-				System.out.println(result);
+		try {
+			Cuenta cb = new Cuenta(20,"5454");
+			
+			int elec = 0;
+			
+			while(elec != 4) {
 				System.out.println("1 Hacer un reintegro. \n"
 						+ "2 Hacer un ingreso \n"
-						+ "3 Consultar el saldo y número de reintegros e ingresos. \n"
+						+ "3 Consultar el saldo y numero de reintegros e ingresos. \n"
 						+ "4 Finalizar las operaciones");
-				elec = teclado.nextInt();
-			}else if(elec == 2){
-				System.out.println("Introduce la cantidad: ");
-				double cantidad = cb.
-			}else if (elec == 3) {
+				 elec = teclado.nextInt();
 				
+				 switch (elec) { 
+				case 1: {
+					double result = cb.getSaldo();
+					System.out.println(result);
+					System.out.println("1 Hacer un reintegro. \n"
+							+ "2 Hacer un ingreso \n"
+							+ "3 Consultar el saldo y numero de reintegros e ingresos. \n"
+							+ "4 Finalizar las operaciones");
+					elec = teclado.nextInt();
+					break;
+				}
+				case 2:{
+					System.out.println("Introduce la cantidad: ");
+					double cantidad = cb.getNumIngresos();
+					System.out.println("1 Hacer un reintegro. \n"
+							+ "2 Hacer un ingreso \n"
+							+ "3 Consultar el saldo y numero de reintegros e ingresos. \n"
+							+ "4 Finalizar las operaciones");
+							elec = teclado.nextInt();		
+							break;
+				}
+				case 3:{
+					double reintegros = cb.getNumReintegros();
+					double ingresos = cb.getNumIngresos();
+					System.out.println("1 Hacer un reintegro. \n"
+							+ "2 Hacer un ingreso \n"
+							+ "3 Consultar el saldo y numero de reintegros e ingresos. \n"
+							+ "4 Finalizar las operaciones");
+					elec = teclado.nextInt();
+					break;
+				}
+				
+				default:
+					throw new IllegalArgumentException("Unexpected value: " + elec);
+				}
 			}
-			
 		}
+		catch(Exception roman){
+			System.out.println(roman.getMessage());
+		}
+		
 	}
 
 }
